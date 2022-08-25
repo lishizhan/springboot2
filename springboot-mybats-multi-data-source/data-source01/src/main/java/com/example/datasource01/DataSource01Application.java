7833300221e351ddef8994166c9891054df4db8d
lishizhan <lishizhan@piesat.cn>
2022-08-25 13:08:22 +0800

    .

---
+++ springboot-mybats-multi-data-source/data-source01/src/main/java/com/example/datasource01/DataSource01Application.java
@@ -0,0 +1,13 @@
+package com.example.datasource01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSource01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSource01Application.class, args);
+    }
+
+}
---
+++ springboot-mybats-multi-data-source/data-source01/src/test/java/com/example/datasource01/DataSource01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datasource01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSource01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
